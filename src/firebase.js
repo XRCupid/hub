d9fe86bf337d226477e2deabf99df14cce0a6c73
Mohamed Abdelnaby <mohamedhanykamal214@gmail.com>
2025-04-14 00:31:41 -0500

    Edit

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c011acb8c6d91dd32d4175a69b2673ef922a971bce9d8c81ceb696760bff8ea
-size 1271
+oid sha256:708d262a0783abed82100b3c248ecea65eaf863b817f88c19ad516e3e5add65a
+size 633
---
+++ src/services/HumeAIService.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4808bc5f8380cffe1cc4dffb26874444b4bf341c0e1168c2d8c7ff209ae116b
+size 8537
