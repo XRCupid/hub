113f0b2e7acb494b5a162c66b05777115d00e5ac
Mohamed Abdelnaby <mohamedhanykamal214@gmail.com>
2025-04-13 20:56:22 -0500

    Edit

--- src/components/VideoChat.js
+++ src/components/VideoChat.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05c0a6fe09c12c5f7ebc960af5d53a859247476b20ba3ca914cb4700ef11318d
-size 11880
+oid sha256:d904094a746a896fd739b0004e7c2b12734ab000c28786ca861fb135f693f04e
+size 13176
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:223230ca67156eb0a2650fe86de4002a7d3559ca95eb193340099f39df838f45
-size 631
+oid sha256:7c011acb8c6d91dd32d4175a69b2673ef922a971bce9d8c81ceb696760bff8ea
+size 1271
