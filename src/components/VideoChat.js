bcfdb91d8db034295614345a40680f975426c9c0
Douglas Goldstein <djg509@nyu.edu>
2025-06-08 14:09:02 -0400

    Clean VideoChat: Remove avatar/coach, keep emotion analysis for dating coaching

--- src/components/VideoChat.js
+++ src/components/VideoChat.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fbcc8dd6ac90a5b182a3b9b8cf658fb450dc1b6d0f4408b0888d4aa7b3e390d
-size 27593
+oid sha256:a16ebb789f0f200a0d62da9b6080b45d1be01cfde3876023fbbcf570289cd494
+size 21727
