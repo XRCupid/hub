5d313d91fa54abeeddaa196cae2aa8141309f422
Douglas Goldstein <djg509@nyu.edu>
2025-06-10 09:50:57 -0700

    Fix mobile room joining - extract room parameter from URL

--- src/components/ConferenceMobile.tsx
+++ src/components/ConferenceMobile.tsx
@@ -1,8 +1,20 @@
-import React, { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import ConferenceBoothDemo from './ConferenceBoothDemo';
 
 const ConferenceMobile: FC = () => {
-  return <ConferenceBoothDemo initialMode="participant" />;
+  const [roomId, setRoomId] = useState<string>('');
+
+  useEffect(() => {
+    // Extract room ID from URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const room = urlParams.get('room');
+    if (room) {
+      console.log('[ConferenceMobile] Found room ID in URL:', room);
+      setRoomId(room);
+    }
+  }, []);
+
+  return <ConferenceBoothDemo initialMode="participant" roomId={roomId} />;
 };
 
 export default ConferenceMobile;
